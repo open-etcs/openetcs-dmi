f6ba9ab76300b580ab97be7887cfca6a247cfb1d
Markus Barenhoff <mbarenh@alios.org>
2015-10-06 08:43:12 +0200

    enabled start button enabling behavior

--- src/ETCS/DMI/MenuWindows.hs
+++ src/ETCS/DMI/MenuWindows.hs
@@ -6,25 +6,55 @@ module ETCS.DMI.MenuWindows
        , menuWinE, menuWinCleanup
        ) where
 
+import           Control.Lens
 import           ETCS.DMI.Button
 import           ETCS.DMI.MenuWindow
+import           ETCS.DMI.Types
 import           FRP.Sodium
 import           GHCJS.DOM.Types     (IsDocument, IsNode)
 
+bsAnd, bsOr :: (Applicative f, Traversable t) => t (f Bool) -> f Bool
+bsAnd = fmap Prelude.and . sequenceA
+bsOr  = fmap Prelude.or . sequenceA
 
-mkMainWindow :: (IsDocument d, IsNode p) => d -> p -> Event Bool -> IO MenuWindow
-mkMainWindow doc parent visible =
+bAnd, bOr :: Applicative f => f Bool -> f Bool -> f Bool
+bAnd a b = (&&) <$> a <*> b
+bOr  a b = (||) <$> a <*> b
+
+
+bStartButtonEnabled i =
+  let bStartEnabled1 = bsAnd
+        [ i ^. trainIsAtStandstill, fmap (SB ==) $ i ^. trainMode
+        , i ^. trainDriverIDIsValid, i ^. trainDataIsValid
+        , i ^. trainLevelIsValid, i ^. trainRunningNumberIsValid
+        ]
+      bStartEnabled2 = bsAnd
+        [ i ^. trainIsAtStandstill, fmap (PT ==) $ i ^. trainMode
+        , i ^. trainDataIsValid
+        , bsOr [ bLevel1
+               , bAnd bLevel23 (fmap not $ i ^. trainHasPendingEmergencyStop)
+               ]
+        ]
+      bStartEnabled3 = bAnd bLevel23 $ fmap (SR ==) $ i ^. trainMode
+      bLevel1  = fmap (Level1 ==) $ i ^. trainLevel
+      bLevel23 = fmap (`elem` [Level2, Level3]) $ i ^. trainLevel
+  in (bStartEnabled1 `bOr` bStartEnabled2) `bOr` bStartEnabled3
+
+
+mkMainWindow :: (IsDocument d, IsNode p) =>
+                TrainBehavior -> d -> p -> Event Bool -> IO MenuWindow
+mkMainWindow i doc parent visible =
   mkMenuWindow doc parent (pure "Main") visible
-  [ (UpButton, pure "Start", pure True)
-  , (UpButton, pure "Driver ID", pure True)
-  , (UpButton, pure "Train Data", pure True)
-  , (UpButton, pure "", pure False)
-  , (UpButton, pure "Level", pure True)
-  , (UpButton, pure "Train running Number", pure True)
-  , (DelayButton, pure "Shunting", pure True)
-  , (DelayButton, pure "Non-Leading", pure True)
-  , (DelayButton, pure "Maintain Shunting", pure False)
-  ]
+     [ (UpButton, pure "Start", bStartButtonEnabled i)
+     , (UpButton, pure "Driver ID", pure True)
+     , (UpButton, pure "Train Data", pure True)
+     , (UpButton, pure "", pure False)
+     , (UpButton, pure "Level", pure True)
+     , (UpButton, pure "Train running Number", pure True)
+     , (DelayButton, pure "Shunting", pure True)
+     , (DelayButton, pure "Non-Leading", pure True)
+     , (DelayButton, pure "Maintain Shunting", pure False)
+     ]
 
 
 mkOverrideWindow :: (IsDocument d, IsNode p) => d -> p -> Event Bool -> IO MenuWindow
--- src/ETCS/DMI/Types.hs
+++ src/ETCS/DMI/Types.hs
@@ -1,13 +1,59 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module ETCS.DMI.Types (
+  ETCSMode (..), ETCSLevel (..), TrainData,
+  NationalValues,
+  TrainBehavior, trainIsAtStandstill, trainMode, trainLevel, trainDriverIDIsValid,
+  trainDataIsValid, trainLevelIsValid, trainRunningNumberIsValid,
+  trainHasPendingEmergencyStop, trainNationalValues, trainHasCommunicationSession,
+  trainIsNonLeading, trainIsPassiveShunting,
   Button, _Button, buttonE, buttonCleanup, ButtonType(..),
   WindowMenuButtonId (..),
   MenuWindow, _MenuWindow, menuWinE, menuWinCleanup
   ) where
 
 import           Control.Lens
+import           Data.Text    (Text)
 import           FRP.Sodium
+
+data ETCSMode = SB | PT | SR | SH | FS | LS | OS | NL| UN | SN
+              deriving (Eq, Show, Ord, Enum, Bounded)
+
+makePrisms ''ETCSMode
+
+data ETCSLevel = Level0 | NTC | Level1 | Level2 | Level3
+               deriving (Eq, Show, Ord, Enum, Bounded)
+
+makePrisms ''ETCSLevel
+
+data TrainData = TrainData
+
+makeLenses ''TrainData
+
+data NationalValues = NationalValues {
+  _natModDriverIDAllowed :: Behavior Bool
+}
+makeLenses ''NationalValues
+
+data TrainBehavior =
+  TrainBehavior {
+    _trainIsAtStandstill          :: Behavior Bool,
+    _trainMode                    :: Behavior ETCSMode,
+    _trainLevel                   :: Behavior ETCSLevel,
+    _trainDriverIDIsValid         :: Behavior Bool,
+    _trainDataIsValid             :: Behavior Bool,
+    _trainLevelIsValid            :: Behavior Bool,
+    _trainRunningNumberIsValid    :: Behavior Bool,
+    _trainHasPendingEmergencyStop :: Behavior Bool,
+    _trainNationalValues          :: Behavior NationalValues,
+    _trainHasCommunicationSession :: Behavior Bool,
+    _trainIsNonLeading            :: Behavior Bool,
+    _trainIsPassiveShunting       :: Behavior Bool
+    }
+
+makeLenses '' TrainBehavior
+
+
 
 data Button t =
   Button {
