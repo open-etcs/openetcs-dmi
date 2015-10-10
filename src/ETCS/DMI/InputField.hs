3af905ee250adf299c6f3a3c941b1a0cff61fac5
Markus Barenhoff <mbarenh@alios.org>
2015-10-10 21:03:40 +0200

    imporved input fields

--- src/ETCS/DMI/InputField.hs
+++ src/ETCS/DMI/InputField.hs
@@ -2,15 +2,12 @@
 
 module ETCS.DMI.InputField where
 
-import           Control.Concurrent
-import           Control.Monad
 import           Data.Text                  (Text)
 import           ETCS.DMI.Helpers
-import           ETCS.DMI.Types
 import           GHCJS.DOM.Element          (setAttribute, setClassName)
 import           GHCJS.DOM.HTMLElement      (setContentEditable, setTitle)
 import           GHCJS.DOM.Node             (appendChild, setTextContent)
-import           GHCJS.DOM.Types            (castToElement, castToHTMLElement)
+import           GHCJS.DOM.Types            (castToElement)
 import           Reactive.Banana
 import           Reactive.Banana.DOM
 import           Reactive.Banana.Frameworks
@@ -19,6 +16,7 @@ import           Reactive.Banana.Frameworks
 
 newtype InputField = InputField ()
 
+mkInputField :: Behavior Text -> Bool -> WidgetInput InputField
 mkInputField = MkInputField
 
 instance IsWidget (InputField) where
@@ -32,10 +30,12 @@ instance IsWidget (InputField) where
     field <- _createDivElement doc
     dataArea <- _createDivElement doc
     setContentEditable dataArea $ pure "true"
-    let fieldWidget =
+    let fieldWidget = do
+          let setFieldTitle = setTitle field
+          valueBLater (_inputLabel i) >>= liftIOLater . setFieldTitle
+          changes (_inputLabel i)  >>= reactimate' . fmap (fmap setFieldTitle)
+
           if _inputIsSingle i then do
-
-
             liftIOLater $ do
               () <$ appendChild field (pure dataArea)
               () <$ appendChild parent (pure field)
@@ -44,15 +44,12 @@ instance IsWidget (InputField) where
             return $ InputField ()
           else do
             labelArea <- liftIO $ _createDivElement doc
-            labelSpan <- liftIO $ _createSpanElement doc
             liftIOLater $ setClassName labelArea "LabelArea"
-            let setLabel l = do
-                  setTextContent labelSpan . pure $ l
+            let setLabel = setTextContent labelArea . pure
             valueBLater (_inputLabel i) >>= liftIOLater . setLabel
             changes (_inputLabel i)  >>= reactimate' . fmap (fmap setLabel)
 
             liftIOLater $ do
-              () <$ appendChild labelArea (pure labelSpan)
               () <$ appendChild field (pure labelArea)
               () <$ appendChild field (pure dataArea)
               () <$ appendChild parent (pure field)
