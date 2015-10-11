fb4035d6df2545c433eb44edb88ce8f197709f3d
Markus Barenhoff <mbarenh@alios.org>
2015-10-11 10:06:16 +0200

    added InputField "Accepted" state

--- src/ETCS/DMI/InputField.hs
+++ src/ETCS/DMI/InputField.hs
@@ -5,7 +5,7 @@ module ETCS.DMI.InputField where
 import           Data.Text                  (Text)
 import           ETCS.DMI.Helpers
 import           GHCJS.DOM.Element          (setAttribute, setClassName)
-import           GHCJS.DOM.HTMLElement      (setContentEditable, setTitle)
+import           GHCJS.DOM.HTMLElement      (setTabIndex, setTitle)
 import           GHCJS.DOM.Node             (appendChild, setTextContent)
 import           GHCJS.DOM.Types            (castToElement)
 import           Reactive.Banana
@@ -16,24 +16,37 @@ import           Reactive.Banana.Frameworks
 
 newtype InputField = InputField ()
 
-mkInputField :: Behavior Text -> Bool -> WidgetInput InputField
+mkInputField :: Behavior Text -> Bool -> Behavior Text -> Behavior Bool ->
+                WidgetInput InputField
 mkInputField = MkInputField
 
 instance IsWidget (InputField) where
   data WidgetInput InputField = MkInputField {
     _inputLabel :: Behavior Text,
-    _inputIsSingle :: Bool
+    _inputIsSingle :: Bool,
+    _inputValue :: Behavior Text,
+    _inputIsAccepted :: Behavior Bool
     }
 
   mkWidgetIO parent i = do
     doc   <- _getOwnerDocument parent
     field <- _createDivElement doc
     dataArea <- _createDivElement doc
-    setContentEditable dataArea $ pure "true"
+    setTabIndex dataArea 1
+
     let fieldWidget = do
           let setFieldTitle = setTitle field
           valueBLater (_inputLabel i) >>= liftIOLater . setFieldTitle
           changes (_inputLabel i)  >>= reactimate' . fmap (fmap setFieldTitle)
+
+          let setDataValue = setTextContent dataArea . pure
+          valueBLater (_inputValue i) >>= liftIOLater . setDataValue
+          changes (_inputValue i)  >>= reactimate' . fmap (fmap setDataValue)
+
+          let setAccepted v =
+                setAttribute dataArea "data-accepted" $ if v then "true" else "false"
+          valueBLater (_inputIsAccepted i) >>= liftIOLater . setAccepted
+          changes (_inputIsAccepted i)  >>= reactimate' . fmap (fmap setAccepted)
 
           if _inputIsSingle i then do
             liftIOLater $ do
